0cf5ab051ea51460d90b9039fe15cd4d1d8e57e0
UmedaMirai <umdmira128@gmail.com>
2022-08-03 12:24:36 +0900

    add: 適当な検索結果を取得する関数を一時的に追加

--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,16 @@
-export const main = () => {
-  console.log(process.env.BEARER_TOKEN)
+import { Client } from "twitter-api-sdk"
+
+const client = new Client(process.env.BEARER_TOKEN as string)
+
+export const main = async () => {
+  try {
+    const resp = await client.tweets.tweetsRecentSearch({
+      query: 'プラハチャレンジ',
+      max_results: 10,
+      expansions: ['author_id']
+    });
+    console.log(resp);
+  } catch (error) {
+    console.log(error);
+  }
 }
