80a9651d88f0d33aea349fe0360c216a0fafc9c6
UmedaMirai <umdmira128@gmail.com>
2022-08-04 11:02:14 +0900

    add: twitter-clientにgetUserByUsernameメソッド、getFollowersByIdメソッドを追加

--- src/domain/interface/twitter-client-interface.ts
+++ src/domain/interface/twitter-client-interface.ts
@@ -2,4 +2,6 @@ import { User } from '@/domain/user/user'
 
 export interface ITwitterClient {
   getUsersByKeyword: (keyword: string) => Promise<User[] | void>
+  getUserByUsername: (username: string) => Promise<User | void>
+  getFollowersById: (userId: string) => Promise<User[] | void>
 }
--- src/infra/twitter-client/twitter-client.ts
+++ src/infra/twitter-client/twitter-client.ts
@@ -31,4 +31,36 @@ export class TwitterClient implements ITwitterClient {
       console.log(error);
     }
   }
+
+  public async getUserByUsername(username: string): Promise<User | void> {
+    try {
+      const resp = await this.client.users.findUserByUsername(username)
+      if (!resp.data) throw new Error('user not found')
+      return new User({
+        id: resp.data.id,
+        name: resp.data.name,
+        username: resp.data.username
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async getFollowersById(userId: string): Promise<User[] | void> {
+    try {
+      const resp = await this.client.users.usersIdFollowers(userId, {
+        max_results: this.MAX_RESULTS
+      })
+      if (!resp.data) return []
+      return resp.data.map(user => {
+        return new User({
+          id: user.id,
+          name: user.name,
+          username: user.username
+        })
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
